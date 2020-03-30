5b7bd7771bf27df68e953513233fea617746fb6a
Gustavo William <62532880+GustavoKristoffersen@users.noreply.github.com>
2020-03-30 11:48:56 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import json
 import bot
-from datetime import date
+from datetime import datetime
+from pytz import timezone
 
-data = date.today()
-data_brasil = '{}/{}/{}'.format(data.day,data.month,data.year)
+data = datetime.now()
+fuso_horario = timezone('America/Sao_Paulo')
+data_e_hora = data.astimezone(fuso_horario)
+data_brasil = data_e_hora.strftime('%d/%m/%Y às %H:%M')
 
 req = Request('https://br.investing.com/currencies/usd-brl', headers={'User-Agent': 'Mozilla/5.0'})
 
@@ -24,13 +27,13 @@ preco_antigo = json.loads(a.read())
 a.close()
 
 if preco > preco_antigo:
-  msg = 'O dólar subiu\U0001F62D - Agora está em R${}\n\nAtualizado em: {}'.format(preco,data_brasil)
+  msg = 'O dólar subiu\U0001F62D - Agora está em R${}\n\nAtualizado em {}'.format(preco,data_brasil)
 
 elif preco < preco_antigo:
-  msg = 'O dólar desceu\U0001F60D - Agora está em R${}\n\nAtualizado em: {}'.format(preco,data_brasil)
+  msg = 'O dólar desceu\U0001F60D - Agora está em R${}\n\nAtualizado em {}'.format(preco,data_brasil)
 
 else:
-  msg = 'O dólar nem subiu nem desceu\U0001F615 \n\nAtualizado em: {}'.format(data_brasil)
+  msg = 'O dólar nem subiu nem desceu\U0001F615 - Agora está em R${} \n\nAtualizado em {}'.format(preco,data_brasil)
 
 bot.enviar_msg(msg)
 
